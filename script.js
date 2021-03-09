18c41d79fc4826d8a4b6d0a7aadb933c9daac301
Andrew <skazlipop.88@mail.ru@>
2021-03-09 15:39:48 +0300

    lesson14.2

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-function DomElement(selector, heightObj, widthObj, bgObj, fontSizeObj){
+function DomElement(selector, heightObj, widthObj, bgObj, fontSizeObj, text){
   this.selector = selector;
   this.heightObj = heightObj;
   this.widthObj = widthObj;
   this.bgObj = bgObj;
   this.fontSizeObj = fontSizeObj;
+  this.text = text;
 }
 
 DomElement.prototype.create = function(){
   if (this.selector[0] === "."){
     let div = document.createElement('div');
     div.className = this.selector.value;
-    div.style.cssText = 'height: heightObj; width: widthObj; background-color: bgObj; font-size: fontSizeObj;';
+    div.style.cssText = `height: ${this.heightObj}; width: ${this.widthObj}; background-color: ${this.bgObj}; font-size:    ${this.fontSizeObj};`;
+    div.textContent = this.text;  
+    document.body.append(div);
   }
   if (this.selector[0] === "#") {
     let div = document.createElement('p');
     div.id = this.selector.value;
-    div.style.cssText = 'height: heightObj; width: widthObj; background-color: bgObj; font-size: fontSizeObj;';
+    div.style.cssText = `height: ${this.heightObj}; width: ${this.widthObj}; background-color: ${this.bgObj}; font-size: ${this.fontSizeObj};`;
+    div.textContent = this.text;
+    document.body.append(div);
   }
 };
 
-let domElement1 = new DomElement('.newElem', '200px', '600px', 'yellow', '20px');
+let domElement1 = new DomElement('.newElem', '200px', '600px', 'yellow', '20px', 'Ура получилось!');
 
 domElement1.create();
